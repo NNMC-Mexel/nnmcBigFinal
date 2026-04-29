f6aeb65abdd682bfa304121b923a9f9abbc61cdc
Bekzat Mukhamedali <bekzatmukhamedali@gmail.com>
2026-04-29 17:08:27 +0500

    fix: cast notification UID to bypass strict TS check until types regenerate

--- pmNNmc/server/src/api/notification/services/notification.ts
+++ pmNNmc/server/src/api/notification/services/notification.ts
@@ -1,3 +1,3 @@
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreService('api::notification.notification');
+export default factories.createCoreService('api::notification.notification' as any);
